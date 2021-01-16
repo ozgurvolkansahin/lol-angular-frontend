3386d2153c2108cd0e7aa4850a69f0a0e744726d
Özgür Volkan Şahin <volkan.sahin@kolaygelsin.com>
2021-01-16 03:44:28 +0300

    footer update

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      Created by <b><a href="https://akveo.page.link/8V2f" target="_blank">pgil</a></b> {{date | date: 'yyyy'}}
     </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
@@ -16,4 +16,5 @@ import { Component } from '@angular/core';
   `,
 })
 export class FooterComponent {
+  date: Date = new Date();
 }
