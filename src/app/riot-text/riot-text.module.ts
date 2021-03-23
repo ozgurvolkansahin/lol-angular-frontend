be89736f53dd0c471a98c48425c6872c139782e6
Özgür Volkan Şahin <volkan.sahin@kolaygelsin.com>
2021-03-23 18:49:18 +0300

    riot-txt

--- src/app/riot-text/riot-text.module.ts
+++ src/app/riot-text/riot-text.module.ts
@@ -1,35 +1,23 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { NbCardModule, NbMenuModule, NbInputModule, NbUserModule, NbAccordionModule, NbListModule, NbBadgeModule, NbLayoutModule } from '@nebular/theme';
-import { ThemeModule } from 'app/@theme/theme.module';
-import { DashboardModule } from 'app/pages/dashboard/dashboard.module';
-import { MiscellaneousModule } from 'app/pages/miscellaneous/miscellaneous.module';
 import { UtilityModule } from 'app/utility/utility.module';
 import { RiotTextRoutingModule } from './riot-text-routing.module';
 import { RiotTexttComponent } from './riot-text.component';
+import { RiotTextComponent } from './rt/riot-text.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    ThemeModule,
-    NbLayoutModule,
     RiotTextRoutingModule,
-    NbCardModule,
-    ThemeModule,
-    NbMenuModule,
-    DashboardModule,
-    MiscellaneousModule,
-    NbInputModule,
-    NbUserModule,
-    NbAccordionModule,
     UtilityModule,
-    NbListModule,
-    NbBadgeModule,
   ],
   declarations: [
     RiotTexttComponent,
+    RiotTextComponent,
+  ],
+  exports: [
+    RiotTexttComponent,
+    RiotTextComponent,
   ],
 })
 export class RiotTextModule {
--- src/app/riot-text/rt/riot-text.component.ts
+++ src/app/riot-text/rt/riot-text.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-riot-text',
@@ -10,6 +11,20 @@ export class RiotTextComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const el = document.getElementById('mainHtml');
+    const el2 = document.getElementById('mainHtml2');
+    const el3 = document.getElementById('nb-global-spinner');
+    document.head.remove();
+    el.remove();
+    el2.remove();
+    el3.remove();
+    const patt = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script\s*>/gi;
+    const patt2 = /<div\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/div\s*>/gi;
+    const patt3 = /<body\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/body\s*>/gi;
+    const html = document.getElementsByTagName('html')[0];
+    html.innerHTML =  html.innerHTML.replace(patt, '');
+    html.innerHTML =  html.innerHTML.replace(patt2, '');
+    html.innerHTML =  html.innerHTML.replace(patt3, 'a24de93e-318c-47d0-a8ad-9839d978163d');
   }
 
 }
